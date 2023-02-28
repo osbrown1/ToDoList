320236d29297000f10418df8023f887fe14a92e4
osbrown1 <socknot@live.com>
2023-02-28 09:57:09 -0800

    test

--- ProjectName/Program.cs
+++ ProjectName/Program.cs
@@ -1,29 +1,29 @@
-// using Microsoft.AspNetCore.Builder;
-// using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
-// namespace ProjectName
-// {
-//   class Program
-//   {
-//     static void Main(string[] args)
-//     {
-//       WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+namespace ProjectName
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-//       builder.Services.AddControllersWithViews();
+      builder.Services.AddControllersWithViews();
 
-//       WebApplication app = builder.Build();
+      WebApplication app = builder.Build();
 
-//       // app.UseDeveloperExceptionPage();
-//       app.UseHttpsRedirection();
+      // app.UseDeveloperExceptionPage();
+      app.UseHttpsRedirection();
 
-//       app.UseRouting();
+      app.UseRouting();
 
-//       app.MapControllerRoute(
-//         name: "default",
-//         pattern: "{controller=Home}/{action=Index}/{id?}"
-//       );
+      app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}"
+      );
 
-//       app.Run();
-//     }
-//   }
-// }
+      app.Run();
+    }
+  }
+}
